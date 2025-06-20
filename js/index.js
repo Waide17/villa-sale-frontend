6d2f5f14e992294337051df640040a55f1f75303
Gabriele Palladino <gabrielepalladino0@gmail.com>
2025-06-20 22:58:19 +0200

    added details.html

--- js/index.js
+++ js/index.js
@@ -41,3 +41,29 @@ hoverElements.forEach((el) => {
     cursor.style.border = "2px solid #facc15"
   })
 })
+
+var swiper = new Swiper(".centered-slide-carousel", {
+  centeredSlides: true,
+  paginationClickable: true,
+  loop: true,
+  spaceBetween: 30,
+  slideToClickedSlide: true,
+  pagination: {
+    el: ".centered-slide-carousel .swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1920: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1028: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    990: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+  },
+})
