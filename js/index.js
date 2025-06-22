15ce33cd9565b694c63a05e30c4f6156c1597d65
Gabriele Palladino <gabrielepalladino0@gmail.com>
2025-06-22 18:31:57 +0200

    Continuata pagina interni

--- js/index.js
+++ js/index.js
@@ -42,28 +42,28 @@ hoverElements.forEach((el) => {
   })
 })
 
-var swiper = new Swiper(".centered-slide-carousel", {
-  centeredSlides: true,
-  paginationClickable: true,
-  loop: true,
-  spaceBetween: 30,
-  slideToClickedSlide: true,
-  pagination: {
-    el: ".centered-slide-carousel .swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    1920: {
-      slidesPerView: 4,
-      spaceBetween: 30,
-    },
-    1028: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    990: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-    },
-  },
-})
+// var swiper = new Swiper(".centered-slide-carousel", {
+//   centeredSlides: true,
+//   paginationClickable: true,
+//   loop: true,
+//   spaceBetween: 30,
+//   slideToClickedSlide: true,
+//   pagination: {
+//     el: ".centered-slide-carousel .swiper-pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     1920: {
+//       slidesPerView: 4,
+//       spaceBetween: 30,
+//     },
+//     1028: {
+//       slidesPerView: 2,
+//       spaceBetween: 10,
+//     },
+//     990: {
+//       slidesPerView: 1,
+//       spaceBetween: 0,
+//     },
+//   },
+// })
